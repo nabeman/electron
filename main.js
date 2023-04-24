5630ba9ad968857e074b51a0afaf755fb8513a34
nabeman <tkmcoins20@gmail.com>
2023-04-24 17:54:46 +0900

    動きます

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ const vueapp = createApp({
     data(){
         return{
             text: "押すと文章が変わります",
+            flag: true,
         }
     },
     methods: {
         changeText(){
+            if(this.flag){
             this.text = "Hello Vue";
+            this.flag = !this.flag;
+        }else{
+            this.text = "押すと文章が変わります";
+            this.flag = !this.flag;
+        }
         }
     }
 });
