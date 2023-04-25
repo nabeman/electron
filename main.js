32620ebc41a1610e23b3450f2f304fa75ff0f823
nabeman <tkmcoins20@gmail.com>
2023-04-25 12:29:35 +0900

    gsapの要素を追加

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const vueapp = createApp({
             text: "Hello Electron",
             flag: true,
             move: false,
+            duration: 1,
+            backgroundcolor: "white",
         }
     },
     methods: {
@@ -13,22 +15,26 @@ const vueapp = createApp({
             if(this.flag){
             this.text = "Hello Vue";
             this.flag = !this.flag;
-        }else{
+            }else{
             this.text = "Hello Electron";
             this.flag = !this.flag;
-        }
+            }
         },
+        changeBoxColor(){
+            gsap.to('.box', { background: this.backgroundcolor, duration: 1.5 });
+        }
+        ,
         moveBox(){
             if(!this.move){
-                gsap.to('.box', { x: 200, duration: 1 });
+                gsap.to('.box', { x: 200, duration: this.duration });
                 this.move = !this.move;
                 console.log('toRight');
             }else{
-                gsap.to('.box', { x: 0, duration: 1 });
+                gsap.to('.box', { x: 0, duration: this.duration });
                 this.move = !this.move;
                 console.log('toLeft');
             }
-        }
+        },
     }
 });
 
